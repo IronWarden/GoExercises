90add5dcfe0f5fe1a4e545dd648b4f5ff82b7081
Rishi Gadhia <gadhiarishi@gmail.com>
2025-05-05 09:05:45 -0400

    added main.go

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func add(x, y int) int {
+	return x + y
+}
+func main() {
+	fmt.Println("Hello, World")
+	fmt.Println(math.Pi)
+	fmt.Println(add(3, 4))
+}
